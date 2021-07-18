fcf7570b9650540ae48b90130ba2b5b8a9a62055
Suraj Chitroda <surajchitroda2@gmail.com>
2021-07-18 17:09:15 +0530

    Added search and add functionalities

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ const express = require('express');
 const exphsb = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
-
 const db = require('./config/database');
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('STARED'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
+app.get('/', (req, res) => res.render('home', { layout: 'landing' }));
 app.use('/jobs', require('./routes/jobs'));
 
-console.log("Authenticating Database");
+
+//set handlebars engine
+app.engine('handlebars', exphsb({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Authenticating Database
 db.authenticate()
     .then(() => console.log("Database authentication successfull"))
     .catch((err) => console.log("ERROR: " + err));
 
 
-app.listen(PORT, console.log(`Server started at port ${PORT}`))
+app.listen(PORT, console.log(`Server started at port ${PORT}`));
 
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,16 +1,73 @@
 const express = require('express');
 const router = express.Router();
+const sequelize = require('sequelize');
+const op = sequelize.Op;
 
 const job = require('../models/jobs');
 
-router.get('/', (req, res) => {
-    job.findAll()
-        .then((data) => {
-            console.log(data);
-            res.sendStatus(200);
-        })
-        .catch((err) => console.log(err));
+// List all jobs
+router.get('/', (req, res) => job.findAll()
+    .then((data) => {
+        res.render('jobs', { data })
+    })
+    .catch((err) => {
+        console.log(err);
+    }));
 
-});
+router.get('/add', (req, res) => res.render('add'))
+
+//add job
+router.post('/add', (req, res) => {
+
+    // get data to insert
+    let { title, technology, description, budget, email } = req.body;
+    let error = "";
+
+    if (!title || !technology || !description || !email) {
+        error = "Provide all values";
+        res.render('add', {
+            error,
+            title,
+            technology,
+            description,
+            budget,
+            email,
+        });
+    }
+    else {
+        if (!budget) {
+            budget = 'unknown'
+        }
+        else {
+            budget = `$${budget}`;
+        }
+
+        technology = technology.toLowerCase();
+
+        // add data to database
+        job.create({
+            title,
+            technology,
+            description,
+            budget,
+            email,
+        })
+            .then((job) => res.redirect('/jobs'))
+            .catch(err => console.log(err));
+    }
+})
+
+router.get('/search', (req, res) => {
+    let { term } = req.query;
+    term = term.toLowerCase();
+    console.log(term);
+    job.findAll({ where: { technology: { [op.like]: '%' + term + '%' } } })
+        .then((job) => {
+            console.log(job);
+            res.render('jobs', { data: job });
+
+        })
+        .catch(err => console.log(err));
+})
 
 module.exports = router;
